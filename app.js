621b2290bdf9e31d19bbc99e00fe5d7a2c56f4b1
Zachery.Sogolow <zachsogolow@Msn.com>
2016-09-26 13:18:34 -0700

    moved websockets out of app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var webApp = require('./server/webapp');
 var NodePi = require('./index');
+var Sockets = require('./sockets');
 
 var app = webApp.createWebApp();
 var settings = {
@@ -8,6 +9,7 @@ var settings = {
 };
 
 var nodePi = new NodePi();
+var sockets = new Sockets(app.server);
 
 app.router.use(function (req, res, next) {
     res.setHeader('test', 'header1');
@@ -37,71 +39,6 @@ app.router.post('/reboot', function (req, res) {
     res.end('rebooting');
 });
 
-function streamUptime(interval) {
-    setInterval(function () {
-        for (var prop in cons) {
-            if (cons.hasOwnProperty(prop)) {
-                var con = cons[prop];
-                con.socket.emit('data', {
-                    type: 'uptime',
-                    data: nodePi.osInfo().uptime
-                });
-            }
-        }
-    }, interval);
-}
-
-streamUptime(1000);
-
-var seed = 0;
-var cons = {};
-var io = require('socket.io')(app.server);
-
-function generateId() {
-    return seed++;
-}
-io.on('connection', function (socket) {
-    var id = generateId();
-    cons[id] = {
-        id: id,
-        socket: socket
-    };
-
-    /**
-     * send the connected event on connection
-     */
-    socket.emit('connected', {
-        hello: 'world'
-    });
-
-    /**
-     * listen for ack message. type will be the message
-     * we are getting the ack from.
-     */
-    socket.on('ack', function (data) {
-        console.log(data.data);
-        switch (data.type) {
-            case 'connected':
-                break;
-            default:
-                break;
-        }
-    });
-
-    /**
-     * listen for \'request\''s
-     */
-    socket.on('request', function (data) {
-        console.log(data.data);
-        switch (data.type) {
-            default: break;
-        }
-    });
-
-    /**
-     * remove the socket from our list of cons
-     */
-    socket.on('disconnect', function () {
-        delete cons[id];
-    });
+sockets.stream(1000, 'all', 'uptime', function () {
+    return nodePi.osInfo().uptime;
 });
---
+++ sockets.js
@@ -0,0 +1,100 @@
+function Sockets(server) {
+    if (this === undefined) {
+        return new Sockets(server);
+    }
+
+    var io = require('socket.io')(server);
+
+    this.seed = 0;
+    this.cons = {};
+
+    var self = this;
+
+    io.on('connection', function (socket) {
+        var id = self.generateId();
+        self.cons[id] = {
+            id: id,
+            socket: socket
+        };
+
+        /**
+         * send the connected event on connection
+         */
+        socket.emit('connected', {
+            hello: 'world'
+        });
+
+        /**
+         * listen for ack message. type will be the message
+         * we are getting the ack from.
+         */
+        socket.on('ack', function (data) {
+            console.log(data.data);
+            switch (data.type) {
+                case 'connected':
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        /**
+         * listen for \'request\''s
+         */
+        socket.on('request', function (data) {
+            console.log(data.data);
+            switch (data.type) {
+                default: break;
+            }
+        });
+
+        /**
+         * remove the socket from our list of cons
+         */
+        socket.on('disconnect', function () {
+            delete self.cons[id];
+        });
+    });
+};
+
+
+Sockets.prototype = {
+    stream: function (interval, socketid, dataType, getData) {
+        if (interval === undefined || isNaN(interval)) {
+            interval = 1000;
+        }
+
+        if (socketid === undefined) {
+            socketid = 'all';
+        }
+
+        var self = this;
+        setInterval(function () {
+            if (socketid === 'all') {
+                for (var prop in self.cons) {
+                    if (self.cons.hasOwnProperty(prop)) {
+                        var con = self.cons[prop];
+                        con.socket.emit('data', {
+                            type: dataType,
+                            data: getData()
+                        });
+                    }
+                }
+            } else {
+                var con = self.cons[socketid];
+                if (con) {
+                    con.socket.emit('data', {
+                        type: dataType,
+                        data: getData()
+                    });
+                }
+            }
+        }, interval);
+    },
+
+    generateId: function () {
+        return this.seed++;
+    }
+}
+
+module.exports = Sockets;
