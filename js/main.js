2424a6eb39c319e9447402b782d1115a93dd3474
zsogolow <zachsogolow@msn.com>
2016-10-02 11:08:28 -0700

    testing

--- js/main.js
+++ js/main.js
@@ -54,31 +54,31 @@ _(document).bind('DOMContentLoaded', function () {
                 break;
 
             case 'duinos':
-                var _lightState = _('#lightState');
-                var lightState = _.http('/lightsState').get();
-                function getStateString(state) {
-                    return state == 0 ? "off" : state == 1 ? "on" : "off"
-                }
-                lightState.then(function (data) {
-                    _lightState.html(getStateString(data));
-                }).catch(function (err) {
-                    console.log(`oops! ${err}`);
-                })
-                _('#offButton').bind('click', function () {
-                    console.log('off');
-                    var lightsOff = _.http('/lightsOff').post();
-                    lightsOff.then(function (data) {
-                        _lightState.html(getStateString(data));
-                    });
-                });
+                // var _lightState = _('#lightState');
+                // var lightState = _.http('/lightsState').get();
+                // function getStateString(state) {
+                //     return state == 0 ? "off" : state == 1 ? "on" : "off"
+                // }
+                // lightState.then(function (data) {
+                //     _lightState.html(getStateString(data));
+                // }).catch(function (err) {
+                //     console.log(`oops! ${err}`);
+                // })
+                // _('#offButton').bind('click', function () {
+                //     console.log('off');
+                //     var lightsOff = _.http('/lightsOff').post();
+                //     lightsOff.then(function (data) {
+                //         _lightState.html(getStateString(data));
+                //     });
+                // });
 
-                _('#onButton').bind('click', function () {
-                    console.log('on');
-                    var lightsOn = _.http('/lightsOn').post();
-                    lightsOn.then(function (data) {
-                        _lightState.html(getStateString(data));
-                    });
-                });
+                // _('#onButton').bind('click', function () {
+                //     console.log('on');
+                //     var lightsOn = _.http('/lightsOn').post();
+                //     lightsOn.then(function (data) {
+                //         _lightState.html(getStateString(data));
+                //     });
+                // });
 
                 break;
             default:
