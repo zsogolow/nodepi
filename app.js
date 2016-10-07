3a88d7a82ee4f830ea4342c218a3214247ccf8ff
Zachery.Sogolow <zachsogolow@Msn.com>
2016-10-07 14:14:26 -0700

    asdfs

--- app.js
+++ app.js
@@ -16,34 +16,55 @@ var nodePi = new NodePi();
 var sockets = new Sockets(app.server);
 var duinos = new Duinos();
 
-var actions = duinos.actions;
-for (var prop in actions) {
-    if (actions.hasOwnProperty(prop)) {
-        var action = actions[prop];
-        var path = `/tmp/${action}`;
+// var actions = duinos.actions;
+// for (var prop in actions) {
+//     if (actions.hasOwnProperty(prop)) {
+//         var action = actions[prop];
+//         var path = `/tmp/${action}`;
 
-        unixSocket(path, function (data) {
-            var dataArray = [];
-            for (var i = 0; i < data.length; i++) {
-                dataArray.push(data[i]);
-            }
-            var id = dataArray[0];
-            var action = dataArray[1];
-            var type = dataArray[2];
-            var extra = dataArray[3];
+//         unixSocket(path, function (data) {
+//             var dataArray = [];
+//             for (var i = 0; i < data.length; i++) {
+//                 dataArray.push(data[i]);
+//             }
+//             var id = dataArray[0];
+//             var action = dataArray[1];
+//             var type = dataArray[2];
+//             var extra = dataArray[3];
 
-            var realType = duinos.getDuinoType(type);
-            var realAction = duinos.getDuinoAction(action);
-            var duino = new Duino(id, realType, realAction, extra);
-            
-            duino.heartbeat = new Date();
+//             var realType = duinos.getDuinoType(type);
+//             var realAction = duinos.getDuinoAction(action);
+//             var duino = new Duino(id, realType, realAction, extra);
 
-            duinos.heartbeat(duino);
+//             duino.heartbeat = new Date();
 
-            sockets.send('all', duino.action, duino);
-        });
+//             duinos.heartbeat(duino);
+
+//             sockets.send('all', duino.action, duino);
+//         });
+//     }
+// }
+var path = '/tmp/heartbeat';
+unixSocket(path, function (data) {
+    var dataArray = [];
+    for (var i = 0; i < data.length; i++) {
+        dataArray.push(data[i]);
     }
-}
+    var id = dataArray[0];
+    var action = dataArray[1];
+    var type = dataArray[2];
+    var extra = dataArray[3];
+
+    var realType = duinos.getDuinoType(type);
+    var realAction = duinos.getDuinoAction(action);
+    var duino = new Duino(id, realType, realAction, extra);
+
+    duino.heartbeat = new Date();
+
+    duinos.heartbeat(duino);
+
+    sockets.send('all', duino.action, duino);
+});
 
 setTimeout(function () {
     console.log("listening now");
